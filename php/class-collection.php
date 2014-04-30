fe1637e2e2a8096d801ec4b39c5f700fe51585ab
Daniel Bachhuber <d@danielbachhuber.com>
2014-04-30 15:42:56 -0700

    Bit more code I haven't hooked up yet

--- php/class-collection.php
+++ php/class-collection.php
@@ -16,4 +16,57 @@ class Collection {
 		$this->post = $post;
 	}
 
+	/**
+	 * Get the ID for this collection
+	 *
+	 * @return int
+	 */
+	public function get_id() {
+		return $this->post->ID;
+	}
+
+	/**
+	 * Get the IDs of posts for the published version of this collection.
+	 *
+	 * @return array
+	 */
+	public function get_published_item_ids() {
+
+		if ( $item_ids = $this->get_meta( 'published_item_ids' ) ) {
+			return $item_ids;
+		} else {
+			return array();
+		}
+
+	}
+
+	/**
+	 * Set thes IDs of posts for the published version of this collection.
+	 *
+	 * @param array
+	 */
+	public function set_published_item_ids( $item_ids ) {
+		$this->set_meta( 'published_item_ids', $item_ids );
+	}
+
+	/**
+	 * Get a meta value for this collection
+	 *
+	 * @param string $key
+	 * @return mixed
+	 */
+	protected function get_meta( $key ) {
+		return get_post_meta( $this->get_id(), $key, true );
+	}
+
+	/**
+	 * Set a meta value for this collection
+	 *
+	 * @param string $key
+	 * @param mixed $value
+	 */
+	protected function set_meta( $key, $value ) {
+		update_post_meta( $this->get_id(), $key, $value );
+	}
+
 }
