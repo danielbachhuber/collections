926be7379bc575a7b693895bb11cb84d0a3ae6c5
Daniel Bachhuber <d@danielbachhuber.com>
2014-05-09 12:27:15 -0700

    Another helpful template tag

--- template-tags.php
+++ template-tags.php
@@ -18,3 +18,22 @@ function collections_get_posts( $name, $args = array() ) {
 
 	return $collection->get_published_items();
 }
+
+/**
+ * Get the IDs for posts in a collections
+ *
+ * @param string $name
+ * @param array $args
+ * @return array
+ */
+function collections_get_post_ids( $name, $args = array() ) {
+
+	do_action( 'collections_get_posts', $name );
+
+	$collection = Post_Collection::get_by_name( $name );
+	if ( ! $collection ) {
+		return array();
+	}
+
+	return $collection->get_published_item_ids();
+}
